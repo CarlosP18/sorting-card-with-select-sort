208d2f68521be153123e1b3198df13bb038e92b2
Carlos Plaza <carlosfplaza@hotmail.com>
2021-01-12 17:46:16 -0300

    se arregla funcion del sorting

--- main.js
+++ main.js
@@ -48,35 +48,41 @@ const orderNumbers = () => {
 }
 
 
+
 const selectSort = (arr) => {
     let min = 0;
-    while (min < arr.length-1){
-        for(let i = min+1; i < arr.length-1; i++) {
-          if (arr[min][0] > arr[i][0]) {
-            let aux = arr[min];
-            arr[min] = arr[i];
-            arr[i] = aux;
-            for (let j = 0; j < arr.length; j++) {
-                let cardDiv = document.createElement("div");
-                let newNumber = arr[j][0];
-                let newSuit = arr[j][1];
-                cardDiv.classList.add("card");
-                cardDiv.classList.add(newSuit);
-                let cardSpan = document.createElement("span");
-                cardSpan.classList.add("number");
-                cardSpan.innerHTML = newNumber === 1 ? "A" : newNumber === 11 ? "J" : newNumber === 12 ? "Q" : newNumber === 13 ? "K" : newNumber;
-                cardDiv.appendChild(cardSpan)
-                let cards = document.querySelector(".cardContainer2")
-                cards.appendChild(cardDiv);
-                if (j == arr.length - 1) {
-                    let br = document.createElement("br");
-                    cards.appendChild(br);
+    while (min < arr.length - 1) {
+        for (let i = min + 1; i < arr.length; i++) {
+            if (arr[min][0] > arr[i][0]) {
+                let aux = arr[min];
+                arr[min] = arr[i];
+                arr[i] = aux;
+                for (let j = 0; j < arr.length; j++) {
+                    let cardDiv = document.createElement("div");
+                    let newNumber = arr[j][0];
+                    let newSuit = arr[j][1];
+                    cardDiv.classList.add("card");
+                    cardDiv.classList.add(newSuit);
+                    let cardSpan = document.createElement("span");
+                    cardSpan.classList.add("number");
+                    cardSpan.innerHTML = newNumber === 1 ? "A" : 
+                    newNumber === 11 ? "J" : 
+                    newNumber === 12 ? "Q" : 
+                    newNumber === 13 ? "K" : 
+                    newNumber;
+                    cardDiv.appendChild(cardSpan)
+                    let cards = document.querySelector(".cardContainer2")
+                    cards.appendChild(cardDiv);
+                    if (j == arr.length - 1) {
+                        let br = document.createElement("br");
+                        cards.appendChild(br);
+                    }
                 }
             }
-          }
         }
+
         min++;
     }
-	return arr;
+    return arr;
 };
 
